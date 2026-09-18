de9ed1c9cb35a2307ec2d712f3e489b05d358990
Sethabile77 <sethabilemzobe17@gmail.com>
2026-09-18 17:18:48 +0200

    modified operator ovrloading file

--- operator_overloading/main.cpp
+++ operator_overloading/main.cpp
@@ -1,34 +1,69 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Bank
 {
 public:
-    void withdrawal(double balance)
+    string last_added_pin;
+    Bank(string pin = "12") : last_added_pin(pin)
     {
-        double amount;
-        cin >> amount;
-        if (amount < balance)
-        {
-            balance = balance - amount;
-        }
+    }
+};
+Bank operator+(const Bank &bank, string pin)
+{
+    Bank absa(bank.last_added_pin);
+    absa.last_added_pin += pin;
+    cout << "You have successfully added the pin : " << absa.last_added_pin << endl;
+    return absa;
+}
+int main()
+{
+
+    Bank result;
+    string pin;
+    cout << "Please enter your new pin : ";
+    cin >> pin;
+    result + pin;
+}
+// prompt user to add the pin,whenever the pin is added it appends the pin to the last pin added
+/*void withdrawal(double balance)
+{
+    double amount;
+    cout << "Please enter the amount ";
+    cin >> amount;
+    if (amount < balance)
+    {
+        balance = balance - amount;
+
         cout << "You have successfully withdrawn: R" << amount << " and the new balance after withdrawal is R" << balance << endl;
     }
-    void withdrawal(double balance, double amount)
+    else
     {
-        if (amount < balance)
-        {
-            balance = balance - amount;
-        }
+        cout << "withdrawal not sucessful: insufficient funds" << endl;
+    }
+}
+void withdrawal(double balance, double amount)
+{
+    if (amount < balance)
+    {
+        balance = balance - amount;
 
         cout << "You have successfully  withdrawn: R" << amount << " and the new balance after withdrawal is R" << balance << endl;
     }
-};
+    else
+    {
+        cout << "withdrawal not successful: insufficient funds" << endl;
+    }
+}*/
 
-int main()
+/*int main()
 {
     Bank result;
     result.withdrawal(1233.09);
-    result.withdrawal(1230.05, 400.50);
+    double temp;
+    cout << "Please enter the amount ";
+    cin >> temp;
+    result.withdrawal(1230.05, temp);
     // cout << "Hello World" << endl;
-}
+}*/
